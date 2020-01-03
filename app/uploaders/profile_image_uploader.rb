4c19bdda89066a825d06cbfdb3c77f1b424d0646
Anubhav Jain <anubhavj1357@gmail.com>
2020-01-04 03:57:42 +0530

    Add validation on file type and size

--- app/uploaders/profile_image_uploader.rb
+++ app/uploaders/profile_image_uploader.rb
@@ -1,2 +1,12 @@
 class ProfileImageUploader < Shrine
+
+  plugin :determine_mime_type
+
+  Attacher.validate do
+    validate_max_size 5 * 1024 * 1024
+    validate_mime_type %w[image/jpeg image/png image/webp]
+    validate_extension %w[jpg jpeg png webp tiff tif]
+  end
+
+
 end
