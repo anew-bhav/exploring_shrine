f3d93add83a0f64582f68d33277c2a0890575e71
Anubhav Jain <anubhavj1357@gmail.com>
2020-01-04 05:27:57 +0530

    Add functionality to create derivatives for profile image

--- app/uploaders/profile_image_uploader.rb
+++ app/uploaders/profile_image_uploader.rb
@@ -1,6 +1,9 @@
+require 'image_processing/mini_magick'
+
 class ProfileImageUploader < Shrine
 
   plugin :determine_mime_type
+  plugin :derivatives
 
   Attacher.validate do
     validate_max_size 5 * 1024 * 1024
@@ -8,5 +11,13 @@ class ProfileImageUploader < Shrine
     validate_extension %w[jpg jpeg png webp tiff tif]
   end
 
+  Attacher.derivatives do |original|
+    magick = ImageProcessing::MiniMagick.source(original)
 
+    {
+      small: magick.resize_to_limit!(300, 300),
+      medium: magick.resize_to_limit!(500, 500),
+      large: magick.resize_to_limit!(800, 800)
+    }
+  end
 end
